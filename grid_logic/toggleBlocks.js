acc3d1a41720aa7b28210d6b3b22573d912d76d4
racoll <racollins1@me.com>
2018-05-23 18:22:57 +0100

    latest

--- grid_logic/toggleBlocks.js
+++ grid_logic/toggleBlocks.js
@@ -1,12 +1,16 @@
 function toggleBlocks(i) {
     for (let x = 1; x < 145; x++) {
       if (!(x % i)) {
-        // window.onload = function(){
           document.getElementById(x.toString()).classList.toggle("block-red");
           console.log(`you clicked block ${i}`)
-        // }
       };
     };
   };
 
   module.exports = toggleBlocks;
+
+
+
+  // if any block is red, disable click event
+  // clikcing the same block turns all white
+  // clicking any other block does nothing
